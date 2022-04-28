9463408e87febfb379e7e719f10fda66059a6017
Binu A <binoo.a@gmail.com>
2022-04-29 00:29:37 +0530

    modified

---
+++ SelectionSort.c
@@ -0,0 +1,59 @@
+// Program - Selection Sort
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+float timedifference_msec(struct timeval t0, struct timeval t1)
+{
+    return (t1.tv_sec - t0.tv_sec) * 1000.0f + (t1.tv_usec - t0.tv_usec) / 1000.0f;
+}
+
+int main(void)
+{
+   struct timeval t0;
+   struct timeval t1;
+   float elapsed;
+   
+   long *data;
+   long num=1000, j;
+   data = (long *)malloc(sizeof(long)*num);
+    
+    int x,y,temp,position;
+
+    if(data != NULL)
+    {
+        for(j = 0; j < num; j++)
+        {
+            data[j] = rand()%100;
+        }
+    }
+    
+   gettimeofday(&t0, NULL);
+
+    for(x = 0; x < num - 1; x++)
+    {
+        position=x;
+        for(y = x + 1; y < num; y++)
+        {
+            if(data[position] > data[y])
+            {
+                position=y;
+            }
+        }
+        if(position != x)
+        {
+            temp=data[x];
+            data[x]=data[position];
+            data[position]=temp;
+        }
+    }
+    
+   gettimeofday(&t1, NULL);
+
+   elapsed = timedifference_msec(t0, t1);
+
+   printf("Code executed in %f milliseconds.\n", elapsed);
+
+   return 0;
+}
